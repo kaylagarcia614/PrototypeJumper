c7ccb5e8967094e2d54fdb21a15644d80f18cdb0
kaylagarcia614 <130188636+kaylagarcia614@users.noreply.github.com>
2023-11-08 15:54:28 -0800

    added sounds and fixed collisions

--- docs/jumper/main.js
+++ docs/jumper/main.js
@@ -12,16 +12,22 @@ const G = {
 options = {
   viewSize: { x: G.WIDTH, y: G.HEIGHT },
   theme: "dark",
-  isReplayEnabled: true
+  isReplayEnabled: true,
+  isCapturing: true,
+  isCapturingGameCanvasOnly: true,
+  captureCanvasScale: 2
 };
 
 let player;
 let enemies = [];
+let maxEnemyCount = 5; // Maximum number of enemies on screen
 let enemyCreationInterval = 60;
 let gameOver = false; // Add a gameOver flag
+let playerDestroyed = false; // Add a playerDestroyed flag
 
 function update() {
-  if (!gameOver) { // Only update the game if it's not over
+  if (!gameOver) {
+	let playerDestroyed = false;
     if (!ticks) {
       player = {
         pos: vec(G.WIDTH * 0.5, G.HEIGHT * 0.5),
@@ -29,9 +35,30 @@ function update() {
       };
     }
 
-    if (ticks % enemyCreationInterval === 0) {
+    if (ticks % enemyCreationInterval === 0 && enemies.length < maxEnemyCount) {
       createEnemy(G.WIDTH, rnd(0, G.HEIGHT), rnd(1, 4));
-      createEnemy(0, rnd(0, G.HEIGHT), -rnd(1, 4));
+      createEnemy(0, rnd(0, G.WIDTH), -rnd(1, 4));
+    }
+
+    if (!playerDestroyed) { // Check if the player is not destroyed
+      player.vy += 0.5;
+      if (player.vy > 2) {
+        player.vy = 2;
+      }
+      player.pos.y += player.vy;
+
+      if (player.pos.y > G.HEIGHT) {
+        player.pos.y = 0;
+      } else if (player.pos.y < 0) {
+        player.pos.y = G.HEIGHT;
+      }
+
+      color("cyan");
+      box(player.pos, 3);
+
+      if (input.isJustPressed) {
+        playerJump();
+      }
     }
 
     // Check for collisions between the player and enemies
@@ -39,41 +66,26 @@ function update() {
       e.pos.x -= e.speed;
       if (e.pos.x < 0 || e.pos.x > G.WIDTH) {
         score++;
+		play("coin");
         return true;
       }
 
-      if (playerHitEnemy(player, e)) {
+      if (!playerDestroyed && playerHitEnemy(player, e)) { // Check if player is not destroyed
+        playerDestroyed = true; // Player is destroyed
         end();
+		play("powerUp"); 
+
+		return true;
       }
 
       color("red");
       box(e.pos, 6, 6);
     });
-
-    player.vy += 0.5;
-    if (player.vy > 2) {
-      player.vy = 2;
-    }
-    player.pos.y += player.vy;
-
-    if (player.pos.y > G.HEIGHT) {
-      player.pos.y = 0;
-    } else if (player.pos.y < 0) {
-      player.pos.y = G.HEIGHT;
-    }
-
-    color("cyan");
-    box(player.pos, 3);
-
-    if (input.isJustPressed) {
-      playerJump();
-    }
   }
 }
 
-// Function to check for collision between the player and an enemy
 function playerHitEnemy(player, enemy) {
-  return player.pos.distanceTo(enemy.pos) < 6; // Adjust the collision radius as needed
+  return player.pos.distanceTo(enemy.pos) < 6;
 }
 
 function playerJump() {
